260d29c7a1f1099167264572f73b972d5321e1c5
eashancode <eashan@geveo.com>
2026-06-12 08:03:22 +0530

    Updates

---
+++ assets/team-pair-DexBBkVN.js
@@ -0,0 +1,1 @@
+const a="/assets/team-standup-CpXkPvKB.jpg",t="/assets/team-review-RbJ7Qlzp.jpg",e="/assets/team-whiteboard-BDeJFnet.jpg",s="/assets/team-pair-Cfjk9UT9.jpg";export{t as a,a as b,s as c,e as t};
